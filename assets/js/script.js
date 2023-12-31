c97cff83510ab35965467fa8ed9341f1a655d809
ilookeeeee43543 <135060768+jkb-bio@users.noreply.github.com>
2023-12-31 12:48:39 +0000

    add files

---
+++ assets/js/script.js
@@ -0,0 +1,114 @@
+let mobile = 'ontouchstart' in document.documentElement;
+
+let switchAllowed = false;
+
+function openSocial(type) {
+  let url = 'about:blank';
+
+  switch (type) {
+    case 'github':
+      url = 'https://github.com/tr1xem';
+      break;
+
+      case 'discord':
+        url = 'https://www.discord.com/users/885063317079592961';
+        break;  
+  }
+
+  window.open(url);
+}
+
+function centerIntroText() {
+  let introText = document.getElementById('intro-text');
+  introText.style.position = 'absolute';
+  introText.style.top = '50%';
+  introText.style.left = '50%';
+  introText.style.transform = 'translate(-50%, -50%)';
+}
+
+function startIntroTyping() {
+  centerIntroText();
+
+  new TypeIt('#intro-text', {
+    speed: 50,
+  })
+    .type('welcome.', { delay: 1200 })
+    .delete(null, { delay: 1000 })
+    .type(`${mobile ? 'tap' : 'press any key'} to enter.`)
+    .go();
+
+  setTimeout(function () {
+    switchAllowed = true;
+  }, 2500);
+}
+
+function typerStartTyping(typer) {
+  typer.reset();
+
+  let text = ['Are You Fken Mirin Brah?', 'Are You Fken Mirin Brah?'];
+
+  text.forEach(function (language, index) {
+    typer.move(null);
+    typer.type(language, { delay: 1000 });
+    typer.pause(1000);
+
+    typer.delete(language.length, { delay: 1000 });
+  });
+
+  typer.go();
+}
+
+function startMainTyping() {
+  let typer = new TypeIt('#subtext', {
+    speed: 50,
+    afterComplete: async () => {
+      typerStartTyping(typer);
+    },
+  });
+
+  typerStartTyping(typer);
+}
+
+function switchScreen() {
+  document.title = 'tr1x_em | home';
+
+  $('.intro').fadeOut(1000, function () {
+    $('.bg-image').fadeIn(1000);
+    $('.main').fadeIn(1000, function () {
+      startMainTyping();
+    });
+  });
+
+  ['background', 'rain'].forEach(function (audioName) {
+    let fullPath = `assets/audio/${audioName}.mp3`;
+
+    let audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', fullPath);
+    audioElement.style.display = 'none';
+
+    audioElement.addEventListener('ended', function () {
+      this.currentTime = 0;
+      this.play();
+    });
+
+    audioElement.play();
+  });
+}
+
+document.addEventListener('keydown', function (e) {
+  if (switchAllowed) {
+    switchAllowed = false;
+    switchScreen();
+  }
+});
+
+document.addEventListener('touchstart', function (e) {
+  if (switchAllowed && mobile) {
+    switchAllowed = false;
+    switchScreen();
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  startIntroTyping();
+});
